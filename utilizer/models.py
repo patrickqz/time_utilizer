4c16b59316fee3982923d1a6296d9674bd058681
meirianto <meiri.anto@gmail.com>
2014-10-18 03:19:33 -0400

    added user and Article models

--- utilizer/models.py
+++ utilizer/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+	TODO: create user model
+
+class Article(models.Model):
+	url = models.charField(max_length = 200)
+	date_added = models.DateTimeField()
+	title = models.CharField(max_length=200)
+	user = models.ForeignKey(User)
+
+	#optional
+	tag = models.CharField()
+	notes = models.TextField()
+
+	def _str_(self):
+		return self.title
+
+	
