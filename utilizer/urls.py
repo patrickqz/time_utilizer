ff2cef1b3e3f14811a5ba7941d9db1c3396b671b
meirianto <meiri.anto@gmail.com>
2014-10-18 07:00:55 -0400

    made url regex for results page

---
+++ utilizer/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+
+from utilizer import views
+
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    url(r'^(/add/)$', views.addLink, name='addLink'),
+    url(r'^/result/(?P<tag>+\s)/(?P<timeToRead>+\d)$', views.getResult, name='getResult')
+]
