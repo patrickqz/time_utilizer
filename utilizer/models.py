63f18170a1dbcbb7ecc929701ae7ea7662c612da
meirianto <meiri.anto@gmail.com>
2014-10-18 03:26:31 -0400

    did some more stuff

--- utilizer/models.py
+++ utilizer/models.py
@@ -3,19 +3,18 @@ from django.db import models
 # Create your models here.
 
 class User(models.Model):
-	TODO: create user model
+	#TODO: create user model
 
 class Article(models.Model):
 	url = models.charField(max_length = 200)
-	date_added = models.DateTimeField()
+	date_added = models.DateTimeField('date added')
 	title = models.CharField(max_length=200)
 	user = models.ForeignKey(User)
 
 	#optional
-	tag = models.CharField()
+	tag = models.CharField(max_length=50)
 	notes = models.TextField()
 
 	def _str_(self):
 		return self.title
 
-	
