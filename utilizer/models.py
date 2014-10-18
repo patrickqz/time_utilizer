2e85246ba538df0a8c165ad77ffa82bb348c1c56
meirianto <meiri.anto@gmail.com>
2014-10-18 05:09:28 -0400

    added some views

--- utilizer/models.py
+++ utilizer/models.py
@@ -7,7 +7,7 @@ class User(models.Model):
 	name = models.charField(max_length=70)
 
 class Article(models.Model):
-	url = models.charField(max_length = 200)
+	url = models.CharField(max_length = 200)
 	date_added = models.DateTimeField('date added')
 	title = models.CharField(max_length=200)
 	user = models.ForeignKey(User)
